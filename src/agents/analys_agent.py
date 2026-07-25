16e51fbb0c71ccdbdbb24c7626b5a4b9afb3563d
musharraf-mac <musharrafcm97@outlook.com>
2026-07-25 23:30:16 +0530

    feat:DB added using chromadb

--- src/agents/analys_agent.py
+++ src/agents/analys_agent.py
@@ -4,8 +4,10 @@ import requests
 from pypdf import PdfReader
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+import chromadb
+from chromadb.utils import embedding_functions 
 
-def download_pdf(pdf_url, save_dir="data/papers",filename=None):
+def download_pdf(pdf_url, save_dir="../../data/papers",filename=None):
     if not pdf_url:
         print("No PDF URL provided.")
         return None
@@ -63,3 +65,18 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 def chunk_text(text, chunk_size=1000, chunk_overlap=200):
     splitter = RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap=chunk_overlap)
     return splitter.split_text(text)
+
+
+# DataBase creation
+client = chromadb.PersistentClient(path="data/vector_db")
+embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2", device="cuda" if torch.cuda.is_available() else "cpu")
+
+collection = client.get_or_create_collection(name="literature_review", embedding_function=embedding_fn)
+
+def store_chunks(chunks, paper_meta, chunk_id_prefix):
+    ids = [f"{chunk_id_prefix}_{i}" for i in range(len(chunks))]
+    metadatas = [{"title": paper_meta["title"], "source": paper_meta["source"]} for _ in chunks]
+    collection.add(documents=chunks, ids=ids, metadatas=metadatas)
+    
+
+
