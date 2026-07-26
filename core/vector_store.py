edb71a3dd1fa9e9f76306be697d1fcd7aa20ee5e
musharraf-mac <musharrafcm97@outlook.com>
2026-07-26 19:47:15 +0530

    fix:changed to chormadb.client() for fix path error

--- core/vector_store.py
+++ core/vector_store.py
@@ -7,8 +7,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 VECTOR_DB_PATH = os.path.join(BASE_DIR, "..", "data","vector_db")
 VECTOR_DB_PATH = os.path.abspath(VECTOR_DB_PATH)
 
-client = chromadb.PersistentClient(path=VECTOR_DB_PATH)
-embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
+# client = chromadb.PersistentClient(path=VECTOR_DB_PATH)
+client = chromadb.Client()
+embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 collection = client.get_or_create_collection(name="literature_review", embedding_function=embedding_fn)
 
